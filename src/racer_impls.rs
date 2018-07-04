29bf7775adc569cccb2b2a79fa2d599fd56d69e0
kngwyu <yuji.kngw.80s.revive@gmail.com>
2018-07-04 22:40:07 +0900

    Remove dependincy to racer

--- src/racer_impls.rs
+++ src/racer_impls.rs
@@ -1,18 +1,15 @@
-extern crate racer;
-
 use std::io;
 use std::path::Path;
 
 use {Vfs, FileContents, Error};
 
-impl racer::FileLoader for Vfs {
-    fn load_file(&self, path: &Path) -> io::Result<String> {
-        match self.0.load_file(path) {
-            Ok(FileContents::Text(t)) => Ok(t),
-            Ok(FileContents::Binary(_)) => Err(
-                io::Error::new(io::ErrorKind::Other, Error::BadFileKind),
-            ),
-            Err(err) => Err(io::Error::new(io::ErrorKind::Other, err)),
-        }
+pub fn racer_load_file(vfs: &Vfs, path: &Path) -> io::Result<String> {
+    match vfs.0.load_file(path) {
+        Ok(FileContents::Text(t)) => Ok(t),
+        Ok(FileContents::Binary(_)) => Err(
+            io::Error::new(io::ErrorKind::Other, Error::BadFileKind),
+        ),
+        Err(err) => Err(io::Error::new(io::ErrorKind::Other, err)),
     }
 }
+
